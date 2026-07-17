6fc27b57218fbce4fce7a783325448b7d34bcd4d
edrinesebuuma2019@gmail.com <edrinesebuuma2019@gmail.com>
2026-07-17 15:26:24 +0300

    chore: adjusted ports

--- back/server.js
+++ back/server.js
@@ -38,6 +38,6 @@ app.get('/', (req, res) => {
 });
 initDataBase();
 // Run Execution Layout
-app.listen(PORT, () => {
+app.listen(PORT, '0,0,0,0', () => {
   console.log(`✔ Server running directly on port ${PORT}`);
 });
