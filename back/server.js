caf0bc4dc3a38c97820bc6d4bbd277fd3a988ea7
edrinesebuuma2019@gmail.com <edrinesebuuma2019@gmail.com>
2026-07-27 22:26:51 +0300

    updated cors

--- back/server.js
+++ back/server.js
@@ -17,8 +17,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
 app.use(express.json()); // Essential payload parsing filtering block
+app.use(cors({origin:"https://smart-agric.vercel.app"}));
 
 // Core Route Mount Points
 app.use('/api/field', fieldRouter);
